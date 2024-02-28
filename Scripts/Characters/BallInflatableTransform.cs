aa4209140e223bbee79798d0d9f2c0562b2f0b9d
naelstrof <naelstrof@gmail.com>
2024-02-28 00:52:25 -0700

    Fixed balls starting at 1.5x scale

--- Scripts/Characters/BallInflatableTransform.cs
+++ Scripts/Characters/BallInflatableTransform.cs
@@ -21,7 +21,7 @@ namespace Naelstrof.Inflatable {
             rig.jiggleSettings = ballBlend;
         }
         public override void OnSizeChanged(float newSize) {
-            ballsRoot.localScale = startScale*Mathf.Max(newSize*0.5f+1f,1f);
+            ballsRoot.localScale = startScale*Mathf.Max(newSize*0.5f+0.5f,1f);
             if (ballBlend != null) {
                 ballBlend.normalizedBlend = Mathf.Clamp01((newSize - 1f) / 2f);
             }
