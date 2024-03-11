2f9a8d3d1bb94a35af95d6d862e2d3bbac2a8988
Archival <ganoosh2@gmail.com>
2024-03-11 20:47:21 +1100

    Added single use bool to list spawner

--- Scripts/GameEvents/GameEventResponseSpawnObjectList.cs
+++ Scripts/GameEvents/GameEventResponseSpawnObjectList.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class GameEventResponseSpawnObjectList : GameEventResponse {
     [SerializeField] private GameObject prefab;
     [SerializeField] private List<Transform> spawnPoints;
+    [SerializeField] private bool singleUse;
 
     public override void Invoke(MonoBehaviour owner) {
         base.Invoke(owner);
         foreach (Transform t in spawnPoints) {
             GameObject.Instantiate(prefab, t.position, Quaternion.identity);
+        }
+        if(singleUse) {
+            spawnPoints.Clear();
         }
     }
 }
