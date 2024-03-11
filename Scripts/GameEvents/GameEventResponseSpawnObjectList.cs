3054eb9d72d677104617e15e8008e425af41ffd0
Archival <ganoosh2@gmail.com>
2024-03-11 19:14:42 +1100

    Added spawn object list event response

---
+++ Scripts/GameEvents/GameEventResponseSpawnObjectList.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameEventResponseSpawnObjectList : GameEventResponse {
+    [SerializeField] private GameObject prefab;
+    [SerializeField] private List<Transform> spawnPoints;
+
+    public override void Invoke(MonoBehaviour owner) {
+        base.Invoke(owner);
+        foreach (Transform t in spawnPoints) {
+            GameObject.Instantiate(prefab, t.position, Quaternion.identity);
+        }
+    }
+}
