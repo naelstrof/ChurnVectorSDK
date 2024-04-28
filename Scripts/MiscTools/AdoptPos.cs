f65585157a03cc80fb270ac4cf979fc3d9830af8
HyperBeast43 <166956231+RhiTheBirdThing@users.noreply.github.com>
2024-04-28 01:18:27 -0500

    tab 2: im a fuckingg idiomnt

--- Scripts/MiscTools/AdoptPos.cs
+++ Scripts/MiscTools/AdoptPos.cs
@@ -3,37 +3,37 @@ using UnityEngine.Events;
 
 public class AdoptPosition : MonoBehaviour
 {
-    public Transform object1; 
-    public bool setx;
-    public bool sety;
-    public bool setz;
-    public Vector3 maxpos;
-    public Vector3 minpos;
+	public Transform object1; 
+	public bool setx;
+	public bool sety;
+	public bool setz;
+	public Vector3 maxpos;
+	public Vector3 minpos;
 
-    public UnityEvent<Transform> onObject1Change;
+	public UnityEvent<Transform> onObject1Change;
 
-    void Update()
-    {
-        Vector3 objtransform = transform.position;
+	void Update()
+	{
+		Vector3 objtransform = transform.position;
 
-        if (setx)
-            objtransform.x = object1.position.x;
+		if (setx)
+			objtransform.x = object1.position.x;
 
-        if (sety)
-            objtransform.y = object1.position.y;
+		if (sety)
+			objtransform.y = object1.position.y;
 
-        if (setz)
-            objtransform.z = object1.position.z;
+		if (setz)
+			objtransform.z = object1.position.z;
 
-        objtransform.x = Mathf.Clamp(objtransform.x, minpos.x, maxpos.x);
-        objtransform.y = Mathf.Clamp(objtransform.y, minpos.y, maxpos.y);
-        objtransform.z = Mathf.Clamp(objtransform.z, minpos.z, maxpos.z);
+		objtransform.x = Mathf.Clamp(objtransform.x, minpos.x, maxpos.x);
+		objtransform.y = Mathf.Clamp(objtransform.y, minpos.y, maxpos.y);
+		objtransform.z = Mathf.Clamp(objtransform.z, minpos.z, maxpos.z);
 
-        transform.position = objtransform;
-    }
+		transform.position = objtransform;
+	}
 
-    public void ChangeObject1(Transform newObject)
-    {
-        object1 = newObject;
-    }
+	public void ChangeObject1(Transform newObject)
+	{
+		object1 = newObject;
+	}
 }
