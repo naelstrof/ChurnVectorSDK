0082f209dda65a8a0f0aaddbaf33cb77c5235291
HyperBeast43 <166956231+RhiTheBirdThing@users.noreply.github.com>
2024-04-28 01:11:37 -0500

    tab

--- Scripts/MiscTools/RandMove.cs
+++ Scripts/MiscTools/RandMove.cs
@@ -3,39 +3,39 @@ using UnityEngine.AI;
 
 public class RandomMovement : MonoBehaviour
 {
-    public Vector3 minPosition = new Vector3(0f, 0f, 0f);
-    public Vector3 maxPosition = new Vector3(0f, 0f, 0f);
-    public float baseMovementInterval = 60f; 
-    public float variance = 10f; 
-    public bool snapToNavmesh = true;
-    private float timer;
-    private NavMeshAgent navMeshAgent;
+	public Vector3 minPosition = new Vector3(0f, 0f, 0f);
+	public Vector3 maxPosition = new Vector3(0f, 0f, 0f);
+	public float baseMovementInterval = 60f; 
+	public float variance = 10f; 
+	public bool snapToNavmesh = true;
+	private float timer;
+	private NavMeshAgent navMeshAgent;
 
-    void Start()
-    {
-        timer = baseMovementInterval;
+	void Start()
+	{
+		timer = baseMovementInterval;
 
-        navMeshAgent = GetComponent<NavMeshAgent>();
+		navMeshAgent = GetComponent<NavMeshAgent>();
 
-        MoveToRandomPosition();
-    }
+		MoveToRandomPosition();
+	}
 
-    void Update()
-    {
-        timer -= Time.deltaTime;
+	void Update()
+	{
+		timer -= Time.deltaTime;
 
-        if (timer <= 0f)
-        {
-            MoveToRandomPosition();
-            timer = baseMovementInterval + Random.Range(-variance, variance);
-        }
-    }
+		if (timer <= 0f)
+		{
+			MoveToRandomPosition();
+			timer = baseMovementInterval + Random.Range(-variance, variance);
+		}
+	}
 
-    void MoveToRandomPosition()
-    {
-        Vector3 randomPosition = new Vector3(Random.Range(minPosition.x, maxPosition.x),
-                                             Random.Range(minPosition.y, maxPosition.y),
-                                             Random.Range(minPosition.z, maxPosition.z));
+	void MoveToRandomPosition()
+	{
+		Vector3 randomPosition = new Vector3(Random.Range(minPosition.x, maxPosition.x),
+											 Random.Range(minPosition.y, maxPosition.y),
+											 Random.Range(minPosition.z, maxPosition.z));
 
 		if (snapToNavmesh = true)
 		{
